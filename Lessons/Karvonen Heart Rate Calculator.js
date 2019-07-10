fa01ea055dee5d6303c441139d775bda083dabfc
Stargazer <mr.kuznetsov@protonmail.ch>
2019-07-11 01:21:46 +0300

    Karvonen Heart Rate Calculator 2

--- Lessons/Karvonen Heart Rate Calculator.js
+++ Lessons/Karvonen Heart Rate Calculator.js
@@ -1,8 +1,8 @@
-*--- Karvonen Heart Rate Calculator ---*
+// Karvonen Heart Rate Calculator
 
 var age = 35;
 var pulseAtRest = 90;
 var intensity = 75;
 var maxHeartRate = 220 - age;
-var pulseAtWorkout = Math.round((maxHeartRate-pulseAtRest)*(intensity/100)+pulseAtRest);
-console.log('Your heart rate is ' + pulseAtWorkout)
+var pulseAtWorkout = Math.round((maxHeartRate - pulseAtRest) * (intensity / 100) + pulseAtRest);
+console.log('Heart rate is ' + pulseAtWorkout);
